e5b1f81282836b226b0c5c9e13ea0f31fe4cab5b
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2023-07-20 12:51:42 -0600

    Graphoics implementarion

---
+++ code/entities.py
@@ -0,0 +1,5 @@
+import pygame
+
+class PhysicalEntity:
+    def __init__(self, game):
+        pass
--- code/main.py
+++ code/main.py
@@ -9,18 +9,25 @@ class Game:
         self.clock = pygame.time.Clock()
 
         #Player Setup
-        self.img = pygame.image.load('../assets/player.png')
+        self.img = pygame.image.load('../assets/Player/Moving/right_0.png')
         self.img.set_colorkey((0, 0, 0))
         self.img_pos = [160, 260]
         self.movement = [False, False]
 
-        self.collision_area = pygame.rect(50, 50, 300, 50)
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
             self.screen.fill(('#000000'))
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.screen.blit(self.img, self.img_pos)
+                
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
