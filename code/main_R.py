7b13a0ca5623cb1e21287abc872a6e2e3f0e10df
Noodle-Dev <92656632+Noodle-Dev@users.noreply.github.com>
2023-07-29 14:55:27 -0600

    Fixed all N-Bugs

--- code/main_R.py
+++ code/main_R.py
@@ -8,32 +8,38 @@ clock = pygame.time.Clock()
 
 screen_width = 1000
 screen_height = 1000
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Bunneta')
 
 icon = pygame.image.load('../assets_img/app_icon.png')
 pygame.display.set_icon(icon)
 
 tile_size = 50
-enemie_group = pygame.sprite.Group()
+enemies_group = pygame.sprite.Group()  # Initialize the enemies_group
 
 def draw_grid():
-	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
                 
 class Player():
     def __init__(self, x, y):
         self.images_right = []
         self.images_left = []
+        self.images_jump = []
         self.index = 0
         self.counter = 0
-        for num in range(1,5):
+        for num in range(1, 5):
             img_right = pygame.image.load((f'../assets_img/Player/player_{num}.png'))
-            img_right = pygame.transform.scale(img_right, (50,80))
+            img_right = pygame.transform.scale(img_right, (50, 80))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
+        for num in range(1, 3):  # Add jump animation frames
+            img_jump = pygame.image.load((f'../assets_img/Player/Jump/jump_{num}.png'))
+            img_jump = pygame.transform.scale(img_jump, (50, 80))
+            self.images_jump.append(img_jump)
+            
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -49,13 +55,22 @@ class Player():
         dx = 0
         w_cooldown = 7
         key = pygame.key.get_pressed()
-        #Jump
-        if key[pygame.K_SPACE] and self.jumped == False:
+
+        # Check if the player is on the ground
+        on_ground = False
+        for tile in world.tile_list:
+            if tile[1].colliderect(self.rect.x, self.rect.y + 1, self.width, self.height):
+                on_ground = True
+                break
+
+        # Jump
+        if key[pygame.K_SPACE] and on_ground and self.jumped == False:
             self.vel_y += -20
             self.jumped = True
         if key[pygame.K_SPACE] == False:
             self.jumped = False
-        #Movement
+
+        # Movement
         if key[pygame.K_LEFT]:
             dx -= 5
             self.counter += 1
@@ -64,7 +79,7 @@ class Player():
             dx += 5
             self.counter += 1
             self.direction = 1
-        
+
         if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
             self.counter = 0
             self.index = 0
@@ -72,22 +87,34 @@ class Player():
                 self.image = self.images_right[self.index]
             if self.direction == -1:
                 self.image = self.images_left[self.index]
-        #anim
+
+        # Animation
         if self.counter > w_cooldown:
             self.counter = 0
             self.index += 1
             if self.index >= len(self.images_right):
-               self.index = 0
+                self.index = 0
             if self.direction == 1:
                 self.image = self.images_right[self.index]
             if self.direction == -1:
                 self.image = self.images_left[self.index]
 
+        # Jump animation
+        if self.jumped:
+            if self.direction == 1:
+                self.image = self.images_jump[0]
+            else:
+                self.image = self.images_jump[1]
+            if self.direction == -1:
+               self.image = pygame.transform.flip(self.image, True, False)
+        
+
         self.vel_y += 1
         if self.vel_y > 10:
             self.vel_y = 10
         dy += self.vel_y
-        #col
+
+        # Collision with tiles
         for tile in world.tile_list:
             if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                 dx = 0
@@ -99,8 +126,8 @@ class Player():
                     dy = tile[1].top - self.rect.bottom
                     self.vel_y = 0
 
-        self.rect.x += dx         
-        self.rect.y += dy 
+        self.rect.x += dx
+        self.rect.y += dy
 
         if self.rect.bottom > screen_height:
             self.rect.bottom = screen_height
@@ -121,66 +148,75 @@ class World():
                 if tile == 1:
                     img = pygame.transform.scale(gn_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = col_count *  tile_size
-                    img_rect.y = row_count *  tile_size
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 2:
                     img = pygame.transform.scale(grass_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = col_count *  tile_size
-                    img_rect.y = row_count *  tile_size
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 3:
-                    enemie = Enemy(col_count * tile_size, row_count * tile_size + 15)
-                    enemie_group.add(enemie)
+                    enemie = Enemy(col_count * tile_size, row_count * tile_size)
+                    enemies_group.add(enemie)  # Add enemies to the enemies_group
                     
                 col_count += 1
             row_count += 1
+
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
-            #pygame.draw.rect(screen, ('#FF0000'), tile[1], 2)
 
 class Enemy(pygame.sprite.Sprite):
-	def __init__(self, x, y):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load('../assets_img/Enemies/enemie_1.png')
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-		self.move_direction = 1
-		self.move_counter = 0
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('../assets_img/Enemies/enemie_1.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))  # Resize the enemy image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - self.rect.height + 50
+        self.move_direction = 1
+        self.move_counter = 0
 
-	def update(self):
-		self.rect.x += self.move_direction
-		self.move_counter += 1
-		if abs(self.move_counter) > 50:
-			self.move_direction *= -1
-			self.move_counter *= -1
-                        
-     
+    def update(self):
+        # Check if there's a ground tile below the enemy
+        on_ground = False
+        for tile in world.tile_list:
+            if tile[1].colliderect(self.rect.x, self.rect.y + 10, self.rect.width, self.rect.height):
+                on_ground = True
+                break
+
+        # If not on the ground, adjust the y-coordinate
+        if not on_ground:
+            self.rect.y += 0
+
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
 
 
 world = World(wd.world_data)
-#enemie_group = pygame.sprite.Group()
-
 player = Player(100, screen_height - 130)
 run = True
-
-#import images
 while run:
     screen.fill('#000000')
     clock.tick(60)
+
     world.draw()
-    enemie_group.update()
-    enemie_group.draw(screen)
+    enemies_group.update()  # Update the enemies
+    enemies_group.draw(screen)  # Draw the enemies
     player.update()
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
     pygame.display.update()
 
 pygame.quit()
