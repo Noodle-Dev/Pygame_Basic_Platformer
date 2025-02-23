0d9034ec5c21792bd9d19f7fda0e82254ac4c472
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2025-02-23 11:21:55 -0600

    Minor changes

--- code/main_R.py
+++ code/main_R.py
@@ -283,8 +283,8 @@ class Lava(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y - self.rect.height + 50
-        #self.move_direction = 1
-        #self.move_counter = 0
+        #self.move_direction = 1 lol
+        #self.move_counter = 0 lol
 
 class Coins(pygame.sprite.Sprite):
     def __init__(self, x, y):
